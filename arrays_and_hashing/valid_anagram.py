f9e21a76f69faf01f62185a72a62c2597b57b2bc
Abulkosim <abulkasimkattaev@gmail.com>
2025-07-21 23:00:00 +0500

    valid anagram - different solution

--- arrays_and_hashing/valid_anagram.py
+++ arrays_and_hashing/valid_anagram.py
@@ -1,3 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return ''.join(sorted(s)) == ''.join(sorted(t))
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map, t_map = {}, {}
+        
+        for i in range(len(s)): 
+            if s[i] not in s_map: 
+                s_map[s[i]] = 1
+            s_map[s[i]] += 1
+
+        for j in range(len(t)): 
+            if t[j] not in t_map: 
+                t_map[t[j]] = 1
+            t_map[t[j]] += 1
+
+        return s_map == t_map
