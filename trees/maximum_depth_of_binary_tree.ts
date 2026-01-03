2a988572c40c10a6d54e3bc7d9dce775e83921bd
Abulkosim <abulkasimkattaev@gmail.com>
2026-01-03 12:49:50 +0500

    maximum depth of binary tree

---
+++ trees/maximum_depth_of_binary_tree.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  const queue: TreeNode[] = [root];
+  let depth = 0;
+
+  while (queue.length > 0) {
+    const levelSize = queue.length; 
+
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift()!; 
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    depth++;
+  }
+
+  return depth;
+}
