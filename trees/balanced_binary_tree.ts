78ea1e694b1870ac88c984c4ca772fdb091b53bd
Abulkosim <abulkasimkattaev@gmail.com>
2026-01-05 23:58:50 +0500

    update: balanced binary tree

--- trees/balanced_binary_tree.ts
+++ trees/balanced_binary_tree.ts
@@ -1,28 +1,24 @@
-class TreeNode {
-  val: number
-  left: TreeNode | null
-  right: TreeNode | null
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = (val === undefined ? 0 : val)
-    this.left = (left === undefined ? null : left)
-    this.right = (right === undefined ? null : right)
-  }
-}
+// class TreeNode {
+//   val: number
+//   left: TreeNode | null
+//   right: TreeNode | null
+//   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//     this.val = (val === undefined ? 0 : val)
+//     this.left = (left === undefined ? null : left)
+//     this.right = (right === undefined ? null : right)
+//   }
+// }
 
 function isBalanced(root: TreeNode | null): boolean {
-  return height(root) !== -1;
-}
+  function check(node: TreeNode | null): number {
+    if (!node) return 0;
+    const leftHeight = check(node.left);
+    if (leftHeight === -1) return -1;
+    const rightHeight = check(node.right);
+    if (rightHeight === -1) return -1;
+    if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
 
-function height(node: TreeNode | null): number {
-  if (!node) return 0;
-
-  const left = height(node.left);
-  if (left === -1) return -1;
-
-  const right = height(node.right);
-  if (right === -1) return -1;
-
-  if (Math.abs(left - right) > 1) return -1;
-
-  return Math.max(left, right) + 1;
+  return check(root) !== -1;
 }
